a934bc9ac411fe3a3fcdbcf1b3e75c0525b166b8
Pezhman <github@pezhmankasraee.com>
2025-02-27 11:57:28 +0100

    add initial

---
+++ src/main/java/com/pezhmankasraee/graph_theory/GraphTheoryApplication.java
@@ -0,0 +1,13 @@
+package com.pezhmankasraee.graph_theory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphTheoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphTheoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pezhmankasraee/graph_theory/GraphTheoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pezhmankasraee.graph_theory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphTheoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
